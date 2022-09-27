1b443690ff1f3817a4aad550a35d23bc514475ef
Angel Marte <robotking134@gmail.com>
2022-09-26 23:35:09 -0400

    commit tree 11abf56cb5c3e743aadd6ac993069f25fa76d8ae

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names,events){
+   const messages = [];
+   for (let i = 0;i < names.length; i ++){
+const message = `Thank you, ${names[i]}, for the wonderful ${events} gift!`
+messages.push(message);
+   }
+   return messages;
+}
+console.log(messages)
+function countDown(){
+    let countDown = 10;
+    while (countDown >= 0){
+        console.log(countDown--)
+    }
+
+}
